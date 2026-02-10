7ce59eb37b8707fa5e56f115e91cf7bd960b7ee3
Andrew Dang-Tran <andrewdt1506@gmail.com>
2026-02-09 23:03:58 -0800

    Small fixes for godot lsp connection and vimrc

--- config.symlink/nvim/after/ftplugin/gdscript.lua
+++ config.symlink/nvim/after/ftplugin/gdscript.lua
@@ -1,12 +1,26 @@
-local port = os.getenv('GDScript_Port') or '6005'
-local cmd = vim.lsp.rpc.connect('127.0.0.1', port)
-local pipe = '/path/to/godot.pipe' -- I use /tmp/godot.pipe
+local port = 6005
+local os_name = vim.loop.os_uname().sysname
 
-vim.lsp.start({
-  name = 'Godot',
+-- In Godot 4, the LSP usually communicates over a port.
+-- We use a pipe/socket connection.
+local cmd = vim.lsp.rpc.connect("127.0.0.1", port)
+
+local config = {
+  name = "Godot",
   cmd = cmd,
   root_dir = vim.fs.dirname(vim.fs.find({ 'project.godot', '.git' }, { upward = true })[1]),
   on_attach = function(client, bufnr)
-    vim.api.nvim_command('echo serverstart("' .. pipe .. '")')
+    -- This helps catch errors during attachment
+    print("LSP Godot attached!")
   end
-})
+}
+
+-- Use pcall (protected call) to prevent Neovim from
+-- throwing a massive error UI if the server isn't running.
+local status, _ = pcall(function()
+    vim.lsp.start(config)
+end)
+
+if not status then
+    print("Godot LSP failed to start. Is the Godot Editor open?")
+end
